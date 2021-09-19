552df47e89aea8c336ae8ea9ee8a0b08b0aa585a
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-09-19 14:24:05 +0200

    fixed menu click

--- javascript.js
+++ javascript.js
@@ -21,28 +21,25 @@ const linkMenuLength = linkMenu.length;
 linkMenu.forEach(element => element.addEventListener("click", (e) => {
   linkMenu.forEach(element => element.classList.remove("active"));
   e.target.classList.add("active");
+  console.log("ok");
+  burgerMenu.classList.remove('active');
+  navbarMenu.classList.remove('active');
+  navbarMenu.removeAttribute("style");
 }));
 
-// Add animation
-const observer = new IntersectionObserver(entries => {
-  // Loop over the entries
-  entries.forEach(entry => {
-    // If the element is visible
-    if (entry.isIntersecting) {
-      // Add the animation class
-      entry.target.classList.add('appear');
-    }
-  });
-});
-
-eachEvents = document.querySelectorAll('.each-event');
-eachEvents.forEach(eachEvent => observer.observe(eachEvent));
-
-
-
-// const wagon = document.getElementById('wagon')
-
-// wagon.addEventListener("click", (e) => {
-//   e.target.innerText +=
+// // Add animation
+// const observer = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('appear');
+//     }
+//   });
 // });
+
+// eachEvents = document.querySelectorAll('.each-event');
+// eachEvents.forEach(eachEvent => observer.observe(eachEvent));
+
 
